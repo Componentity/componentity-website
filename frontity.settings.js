3eab9c2d722bc92b5d2b1f62a4d3f7dcb52a167c
ymakarim <ymakarim@gmail.com>
2020-11-11 13:44:43 +0430

    url changed

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,59 +1,48 @@
 const settings = {
-  "name": "Componentity - From CodeNawis",
-  "state": {
-    "frontity": {
-      "url": "https://componentity.com",
-      "title": "Componentity",
-      "description": "Thousands of Components Ready to be Copy-Pasted"
-    }
+  name: "Componentity - From CodeNawis",
+  state: {
+    frontity: {
+      url: "https://componentity.com",
+      title: "Componentity",
+      description: "Thousands of Components Ready to be Copy-Pasted",
+    },
   },
-  "packages": [
+  packages: [
     {
-      "name": "@frontity/codenawis",
-      "state": {
-        "theme": {
-          "menu": [
-            [
-              "Home",
-              "/"
-            ],
-            [
-              "Support Us",
-              "/support"
-            ],
-            [
-              "Blog",
-              "/category/blog"
-            ],
-            [
-              "Components",
-              "https://componentity.com/#getStarted"
-            ],
+      name: "@frontity/codenawis",
+      state: {
+        theme: {
+          menu: [
+            ["Home", "/"],
+            ["Support Us", "/support"],
+            ["Blog", "/category/blog"],
+            ["Components", "https://componentity.com/#getStarted"],
           ],
-          "featured": {
-            "showOnList": true,
-            "showOnPost": true
-          }
-        }
-      }
+          featured: {
+            showOnList: true,
+            showOnPost: true,
+          },
+        },
+      },
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "https://codenawis.com/componentity/wp-json",
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api:
+            "https://public-api.wordpress.com/wp/v2/sites/comoponentity.wordpress.com/",
           params: {
-            per_page: 20
+            per_page: 20,
           },
-        }
-      }
+        },
+      },
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
     "frontity-share",
     "frontity-contact-form-7",
-    "@frontity/head-tags"
-  ]
+    "@frontity/head-tags",
+  ],
 };
 
 export default settings;
